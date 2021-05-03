d40259581d46fe1babcb7a899bc6c96f040368ee
mateusnobre <mateusnobrests@gmail.com>
2021-05-03 20:52:48 -0300

    mobile block

--- source/gameplay/components/platforms/block.py
+++ source/gameplay/components/platforms/block.py
@@ -53,7 +53,7 @@ class Block(Component):
 
 class MobileBlock(Block):
 
-    def __init__(self, x, y, angle, pixels_x, pixels_y, y_velocity = 10):
+    def __init__(self, x, y, angle, pixels_x, pixels_y, y_velocity = 5):
         super().__init__(x, y, angle, pixels_x, pixels_y)
         self.img = pygame.transform.scale(self.img, (pixels_x, pixels_y))
         self.side_img1 = pygame.transform.rotate(SIDE, 90)
@@ -67,7 +67,7 @@ class MobileBlock(Block):
 
     def draw(self, win):
         _, max_y = win.get_size()
-        if ((self.y + self.y_velocity + self.pixels_y >= max_y - 30) or (self.y + self.y_velocity <= 30)):
+        if ((self.y + self.y_velocity + self.pixels_y >= max_y - 60) or (self.y + self.y_velocity <= 60)):
             self.y_velocity *= -1
         self.y += self.y_velocity
         win.blit(self.img, (self.x, self.y))
@@ -92,3 +92,52 @@ class MobileBlock(Block):
 
         return False
 
+class MobilePortal(Block):
+    def __init__(self, x, y, angle, pixels_x, pixels_y, y_velocity = 0):
+        super().__init__(x, y, angle, pixels_x, pixels_y)
+        self.y_velocity = y_velocity
+        self.pixels_y = pixels_y
+        self.pixels_x = pixels_x
+        max_y = 680
+        self.y = max_y/2
+        self.upper_img = pygame.transform.scale(self.img, (self.pixels_x, int((max_y - self.pixels_y)/2)))
+        self.upper_side_img = pygame.transform.rotate(SIDE, 90)
+        self.upper_side_img = pygame.transform.scale(self.upper_side_img, (20, int((max_y - self.pixels_y)/2)))
+
+        self.lower_img = pygame.transform.scale(self.img, (self.pixels_x, int((max_y - self.pixels_y)/2)))
+
+    def draw(self, win):
+        _, max_y = win.get_size()
+        if ((self.y + self.y_velocity + self.pixels_y/2 >= max_y - 60) or (self.y + self.y_velocity <= 60 + self.pixels_y/2)):
+            self.y_velocity *= -1
+        self.y += self.y_velocity
+
+        self.upper_img = pygame.transform.scale(self.img, (self.pixels_x, int(self.y - self.pixels_y/2)))
+        self.lower_img = pygame.transform.scale(self.img, (self.pixels_x, int(max_y - self.y - self.pixels_y/2)))
+        
+        self.upper_side_img = pygame.transform.rotate(SIDE, 90)
+        self.upper_side_img = pygame.transform.scale(self.upper_side_img, (20, int(self.y - self.pixels_y/2)))
+        
+        self.lower_side_img = pygame.transform.rotate(SIDE, 90)
+        self.lower_side_img = pygame.transform.scale(self.lower_side_img, (20, int(max_y - self.y - self.pixels_y/2)))
+        
+        win.blit(self.upper_img, (self.x, 0))
+        win.blit(self.lower_img, (self.x, max_y - (int(max_y - self.y - self.pixels_y/2))))
+        win.blit(self.upper_side_img, (self.x - self.upper_side_img.get_width(), 0))
+        win.blit(self.lower_side_img, (self.x - self.lower_side_img.get_width(), max_y - (int(max_y - self.y - self.pixels_y/2))))
+
+    def collision(self, player):
+        player_mask = player.get_mask()
+        lower_mask = pygame.mask.from_surface(self.lower_img)
+        lower_offset = (round(self.x - player.x), round(self.y - player.y))
+        lower_point = player_mask.overlap(lower_mask, lower_offset)
+
+
+        upper_mask = pygame.mask.from_surface(self.upper_img)
+        upper_offset = (round(self.x - player.x), round(self.y - player.y))
+        upper_point = player_mask.overlap(upper_mask, upper_offset)
+        
+        if lower_point or upper_point:
+            return True
+
+        return False
