e5b3741a08ebeb4867f47d6e55c1b181c8e08dda
Eneas <lucasinamine95@gmail.com>
2021-05-03 18:57:08 -0300

    Update menu.py

--- source/gameplay/essentials/menu.py
+++ source/gameplay/essentials/menu.py
@@ -1,7 +1,6 @@
-## ENEAS NEW ADDITION (03/04 23:34)
-    
 import pygame
-from ...graphics.images_loader import BG
+from ...graphics.images_loader import *
+import source.audio.audio_loader as al
 
 WIN_WIDTH = 1000
 WIN_HEIGHT = 680
@@ -17,19 +16,20 @@ class Menu():
         self.offset = -120 #cursor
         self.WHITE = (255,255,255)
         self.BLACK = (0,0,0)
+        self.YELLOW = (255,255,0)
         self.font_name = 'freesansbold.ttf'
         self.chooselvl = 1
     
-    def draw_text(self, text, size, x, y):
+    def draw_text(self, text, size, x, y, color):
         font = pygame.font.Font(self.font_name,size)
-        text_surface = font.render(text, True, self.WHITE)
+        text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
         self.window.blit(text_surface, text_rect)
         
 
     def draw_cursor(self):
-        self.draw_text('>', 40, self.cursor_rect.x, self.cursor_rect.y)
+        self.draw_text('>', 40, self.cursor_rect.x, self.cursor_rect.y, self.WHITE)
 
 
 class MainMenu(Menu):
@@ -37,7 +37,7 @@ class MainMenu(Menu):
         Menu.__init__(self, checkinputs)
         self.state = "Start"
         self.startx, self.starty = self.mid_width, self.mid_height + 50
-        #Volume
+        #Help
         self.volumex, self.volumey = self.mid_width, self.mid_height + 100
         #Choose Map
         self.mapx, self.mapy = self.mid_width, self.mid_height + 150
@@ -49,10 +49,10 @@ class MainMenu(Menu):
             self.checkinputs.check_events()
             self.check_input()
             self.window.blit(BG, (0,0))
-            self.draw_text('Main Menu', 40, WIN_WIDTH/2, WIN_HEIGHT/2 - 20)
-            self.draw_text("Start Game", 40, self.startx, self.starty)
-            self.draw_text("Volume", 40, self.volumex, self.volumey)
-            self.draw_text("Choose Map", 40, self.mapx, self.mapy)
+            self.draw_text('Main Menu', 40, WIN_WIDTH/2, WIN_HEIGHT/2 - 20, self.WHITE)
+            self.draw_text("Start Game", 40, self.startx, self.starty, self.WHITE)
+            self.draw_text("Help", 40, self.volumex, self.volumey, self.WHITE)
+            self.draw_text("Choose Map", 40, self.mapx, self.mapy, self.WHITE)
             self.draw_cursor()
             pygame.display.update()
             self.checkinputs.reset_keys()
@@ -86,7 +86,7 @@ class MainMenu(Menu):
             if self.state == 'Start':
                 self.checkinputs.playing = True
             elif self.state == 'Volume':
-                pass
+                self.checkinputs.curr_menu = self.checkinputs.helpmenu
             elif self.state == 'Choose Map':
                 self.checkinputs.curr_menu = self.checkinputs.choose_map
             self.run_display = False
@@ -106,10 +106,10 @@ class ChooseMapMenu(Menu):
             self.checkinputs.check_events()
             self.check_input2()
             self.window.blit(BG, (0,0))
-            self.draw_text('Choose Level', 40, WIN_WIDTH/2, WIN_HEIGHT/2 - 20)
-            self.draw_text("Level 1", 40, self.map1x, self.map1y)
-            self.draw_text("Level 2", 40, self.map2x, self.map2y)
-            self.draw_text("Level 3", 40, self.map3x, self.map3y)
+            self.draw_text('Choose Level', 40, WIN_WIDTH/2, WIN_HEIGHT/2 - 20, self.WHITE)
+            self.draw_text("Level 1", 40, self.map1x, self.map1y, self.WHITE)
+            self.draw_text("Level 2", 40, self.map2x, self.map2y, self.WHITE)
+            self.draw_text("Level 3", 40, self.map3x, self.map3y, self.WHITE)
             self.draw_cursor()
             pygame.display.update()
             self.checkinputs.reset_keys()
@@ -145,6 +145,7 @@ class ChooseMapMenu(Menu):
         self.move_cursor2()
         if self.checkinputs.START_KEY:
             if self.state == 'Level 1':
+                self.chooselvl = 1
                 self.checkinputs.playing = True
             elif self.state == 'Level 2':
                 self.chooselvl = 2
@@ -153,4 +154,48 @@ class ChooseMapMenu(Menu):
                 self.chooselvl = 3
                 self.checkinputs.playing = True
             self.run_display = False
+
+
+class HelpMenu(Menu):
+    def __init__(self, checkinputs):
+        Menu.__init__(self, checkinputs)
+        self.map1x, self.map1y = self.mid_width, self.mid_height - 50
+        self.map2x, self.map2y = self.mid_width, self.mid_height 
+        self.map3x, self.map3y = self.mid_width, self.mid_height + 50
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.checkinputs.check_events()
+            self.check_input3()
+            self.window.blit(BG, (0,0))
+            self.window.blit(CUBE, (WIN_WIDTH/2-450, self.map1y - 20))
+            self.window.blit(DRAGON, (WIN_WIDTH/2-480, self.map1y +60))
+            self.window.blit(ORB, (WIN_WIDTH/2-450, self.map1y +140))
+            self.window.blit(LASER, (WIN_WIDTH/2-435, self.map1y +220))
+            self.draw_text('Help', 50, WIN_WIDTH/2, WIN_HEIGHT/2 - 280, self.YELLOW)
+            self.draw_text("Ultimate Runners é um jogo de plataforma com interação por meio de toque/clique",20, self.map1x, self.map1y -160, self.WHITE)
+            self.draw_text("para controlar diferentes personagens, que reagem deformas únicas quando ",20, self.map1x, self.map1y -140, self.WHITE)
+            self.draw_text("pressionada a tecla ESPAÇO no teclado.",20,self.map1x, self.map1y -120, self.WHITE)
+            self.draw_text("Você deve utilizar constantemente essa tecla para desviar dos obstáculos no decorrer de cada nível.", 20, self.map1x, self.map1y - 100,  self.WHITE)
+            self.draw_text("Pressione BACKSPACE para retornar ao menu principal.",20,self.map1x, self.map1y -80, self.WHITE)
+            self.draw_text("CUBO", 20, self.map1x, self.map1y, self.YELLOW)
+            self.draw_text("pode realizar pequenos saltos, quando em contato com a superfície;", 20, self.map1x, self.map1y + 20,  self.WHITE)
+            self.draw_text("após o salto, ele cai pela ação da gravidade.", 20, self.map1x, self.map1y + 40, self.WHITE)
+            self.draw_text("DRAGÃO", 20, self.map1x, self.map1y + 80, self.YELLOW)
+            self.draw_text("pode ser impulsionado para cima de forma gradativa,", 20, self.map1x, self.map1y + 100, self.WHITE)
+            self.draw_text("caindo lentamente pela ação da gravidade.", 20, self.map1x, self.map1y + 120, self.WHITE)
+            self.draw_text("ORBE", 20, self.map1x, self.map1y + 160, self.YELLOW)
+            self.draw_text("pode se mover abruptamente por alternância da gravidade,", 20, self.map1x, self.map1y + 180, self.WHITE)
+            self.draw_text("quando em contato com a superfície.", 20, self.map1x, self.map1y + 200, self.WHITE)
+            self.draw_text("LASER", 20, self.map1x, self.map1y + 240, self.YELLOW)
+            self.draw_text("pode subir e descer rapidamente em ângulos mais", 20, self.map1x, self.map1y + 260, self.WHITE)
+            self.draw_text("íngremes.", 20, self.map1x, self.map1y + 280, self.WHITE)
+            pygame.display.update()
+            self.checkinputs.reset_keys()
+            pygame.display.flip()
     
+    def check_input3(self):
+        if self.checkinputs.BACK_KEY:
+            self.checkinputs.curr_menu = self.checkinputs.main_menu
+            self.run_display = False
